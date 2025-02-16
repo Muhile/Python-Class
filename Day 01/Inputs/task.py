f19060046618c106cfe9f011efd285f1e17e5448
Muhile Hansen <hanscheng94@gmail.com>
2025-02-16 17:06:14 -0600

    Day 01 learning tasks

---
+++ Day 01/Inputs/task.py
@@ -0,0 +1,2 @@
+# Input () function, a prompt for the user, to collect user input and use it within your code.
+input("What is your name?")
---
+++ Day 01/String Manipulation and Code Intelligence/task.py
@@ -0,0 +1,10 @@
+# Write your code below this line
+print("Hello world!\nHello world!")
+print("Test01\ntest02")
+print("test03\n test04")
+print("Hello world!\nHello world!\nHello World!")
+
+# String Concatination
+print("Hello" + "Hansen")
+print("Hello" + " Hansen")
+print("Hello" + " " + "Hansen")
