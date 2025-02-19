4f75decd6a5b3c5c10c1bba3790a4d4f9eee48ad
Muhile Hansen <hanscheng94@gmail.com>
2025-02-19 16:54:02 -0600

    Still learning day 01

--- Day 01/Inputs/task.py
+++ Day 01/Inputs/task.py
@@ -1,2 +1,5 @@
 # Input () function, a prompt for the user, to collect user input and use it within your code.
-input("What is your name?")
+# input("What is your name?")
+
+# This line of code will take an input using the input() function
+print("Hello " + input("What is your name?") + "!")
---
+++ Day 01/Variable Naming/task.py
@@ -0,0 +1,18 @@
+# Variables is a concept in programming that allows us to give a label to a piece of data so that we can refer or reference that data using the chosen variable name
+# Here we will see  how to create variables and how to use the variables to access the contained value
+
+
+# This line of code will take an input using the input() function and assign that to the variable name
+# name = input("What is your name?")
+# print(name)
+
+# name = "Jack"
+# print(name)
+
+# name = "Angela"
+# print(name)
+
+# number = input("What is your input?")
+# print(len(number))
+
+print(len(input("What is your input?")))
